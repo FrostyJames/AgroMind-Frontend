ec4f98830f91f8d7f86b009d360e7f3d930f1dc9
James Ivan <ivanlavan773@gmail.com>
2025-10-20 06:14:50 +0300

    modified

--- AgroMind-Frontend/src/App.jsx
+++ AgroMind-Frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Recommendations from "./pages/Recommendations";
+import Schedule from "./pages/Schedule";
+import Activities from "./pages/Activities";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/recommendations" element={<Recommendations />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/activities" element={<Activities />} />
       </Routes>
     </Router>
   );
